5247ff807a0d4ee175eb3d9d6bd7b64b7347b71e
debanjan-bhuinya <debanjanbhuinya4521@gmail.com>
2026-03-26 14:03:04 +0530

    Day 78: Added vulnerable file for AI testing

---
+++ vulnerable.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+def get_user_data(username):
+    # Extremely dangerous SQL Injection vulnerability!
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = '" + username + "'")
+    return cursor.fetchall()
